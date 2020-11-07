aab8fcb1057bb9b870023c56ec3f31b791679c66
staugur <staugur@saintic.com>
2020-11-07 13:31:51 +0000

    Deploying to gh-pages from  @ 15294720c0d382f59e97acd1de87a94a67bb37da 🚀

--- baidu.js
+++ baidu.js
@@ -1,4 +1,5 @@
 ;(function() {
+    //baidu autopush
     var bp = document.createElement('script')
     var curProtocol = window.location.protocol.split(':')[0]
     if (curProtocol === 'https') {
@@ -8,4 +9,10 @@
     }
     var s = document.getElementsByTagName('script')[0]
     s.parentNode.insertBefore(bp, s)
+    //baidu statictis
+    var _hmt = _hmt || []
+    var hm = document.createElement('script')
+    hm.src = 'https://hm.baidu.com/hm.js?88f6d909dd0d8f80bb9748f43ade3ac7'
+    var s = document.getElementsByTagName('script')[0]
+    s.parentNode.insertBefore(hm, s)
 })()
