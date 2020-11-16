5cd3d01ceb3ef955b5a35f6a0260caa4018c8d4f
Hiroshi.tao <me@tcw.im>
2020-11-16 21:34:24 +0800

    add gen go module tpl

---
+++ newGoModule.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from sys import argv, version_info
+from os import makedirs
+from os.path import isdir, abspath, dirname, join
+
+PY2 = version_info[0] == 2
+tpl = """<!DOCTYPE html>
+<html>
+<head>
+    <meta title="tcw.im/{name}">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="go-import" content="tcw.im/{name} git https://github.com/staugur/{name}">
+    <meta http-equiv="refresh" content="0; url=https://pkg.go.dev/tcw.im/{name}">
+</head>
+<body>
+    Redirecting to docs at <a href="https://pkg.go.dev/tcw.im/{name}">pkg.go.dev/tcw.im/{name}</a>...
+</body>
+</html>
+"""
+
+if __name__ == "__main__":
+    try:
+        name = argv[1]
+    except IndexError:
+        print("usage: {} go-module-name".format(argv[0]))
+    else:
+        pub = join(dirname(abspath(__file__)), ".vuepress", "public", name)
+        print(tpl.format(name=name))
+        try:
+            raw_input() if PY2 else input()
+        except KeyboardInterrupt:
+            pass
+        else:
+            if not isdir(pub):
+                makedirs(pub)
+            with open(join(pub, "index.html"), "w") as fp:
+                fp.write(tpl.format(name=name))
