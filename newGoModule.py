f880276bb27836e758e471a102034af0d821f1d7
Hiroshi.tao <me@tcw.im>
2021-01-04 17:06:47 +0800

    new go pkg: ufc

--- newGoModule.py
+++ newGoModule.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from sys import argv, version_info
+from sys import argv, version_info, exit
 from os import makedirs
 from os.path import isdir, abspath, dirname, join
 
@@ -10,7 +10,7 @@ tpl = """<!DOCTYPE html>
 <head>
     <meta title="tcw.im/{name}">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="go-import" content="tcw.im/{name} git https://github.com/staugur/{name}">
+    <meta name="go-import" content="tcw.im/{name} git https://github.com/staugur/{pkg}">
     <meta http-equiv="refresh" content="0; url=https://pkg.go.dev/tcw.im/{name}">
 </head>
 <body>
@@ -24,15 +24,19 @@ if __name__ == "__main__":
         name = argv[1]
     except IndexError:
         print("usage: {} go-module-name".format(argv[0]))
+        exit(1)
+    try:
+        pkg = argv[2]
+    except IndexError:
+        pkg = name
+    pub = join(dirname(abspath(__file__)), ".vuepress", "public", name)
+    print(tpl.format(name=name, pkg=pkg))
+    try:
+        raw_input() if PY2 else input()
+    except KeyboardInterrupt:
+        pass
     else:
-        pub = join(dirname(abspath(__file__)), ".vuepress", "public", name)
-        print(tpl.format(name=name))
-        try:
-            raw_input() if PY2 else input()
-        except KeyboardInterrupt:
-            pass
-        else:
-            if not isdir(pub):
-                makedirs(pub)
-            with open(join(pub, "index.html"), "w") as fp:
-                fp.write(tpl.format(name=name))
+        if not isdir(pub):
+            makedirs(pub)
+        with open(join(pub, "index.html"), "w") as fp:
+            fp.write(tpl.format(name=name, pkg=pkg))
