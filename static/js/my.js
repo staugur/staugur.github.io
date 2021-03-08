dea2cbbaa5f713fa5fe53d60685268470910f07d
Hiroshi.tao <me@tcw.im>
2021-03-08 20:24:52 +0800

    update js

--- static/js/my.js
+++ static/js/my.js
@@ -1,49 +1,4 @@
-;(function() {
-    //tools
-    function ajax(options) {
-        options = options || {}
-        options.type = (options.type || 'GET').toUpperCase()
-        options.dataType = options.dataType || 'json'
-        var params = formatParams(options.data)
-        if (window.XMLHttpRequest) {
-            var xhr = new XMLHttpRequest()
-        } else {
-            var xhr = new ActiveXObject('Microsoft.XMLHTTP')
-        }
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4) {
-                var status = xhr.status
-                if (status >= 200 && status < 300) {
-                    options.success &&
-                        options.success(xhr.responseText, xhr.responseXML)
-                } else {
-                    options.fail && options.fail(status)
-                }
-            }
-        }
-        if (options.type == 'GET') {
-            xhr.open('GET', options.url + '?' + params, true)
-            xhr.send(null)
-        } else if (options.type == 'POST') {
-            xhr.open('POST', options.url, true)
-            xhr.setRequestHeader(
-                'Content-Type',
-                'application/x-www-form-urlencoded'
-            )
-            xhr.send(params)
-        }
-    }
-    function formatParams(data) {
-        var arr = []
-        for (var name in data) {
-            arr.push(
-                encodeURIComponent(name) + '=' + encodeURIComponent(data[name])
-            )
-        }
-        arr.push(('v=' + Math.random()).replace('.', ''))
-        return arr.join('&')
-    }
-
+;(function () {
     //baidu autopush
     var bp = document.createElement('script')
     var curProtocol = window.location.protocol.split(':')[0]
@@ -61,5 +16,4 @@
     hm.src = 'https://hm.baidu.com/hm.js?ccc146f638178f462dc741559602236c'
     var s = document.getElementsByTagName('script')[0]
     s.parentNode.insertBefore(hm, s)
-
 })()
